c85379b743bd01ffda757bd70ead679dde5f496a
Conor Twamley <ctwamley@gmail.com>
2019-04-09 18:37:47 +0100

    Formatting class added to the examples

---
+++ FirstUnit Testing/src/formatting/FormattingExamples.java
@@ -0,0 +1,41 @@
+package formatting;
+
+/*
+* There should be a short description at the beginning of 
+* every class like this telling us what the purpose of the 
+* class is. Use the block commenting style.
+*/
+
+public class FormattingExamples {
+	
+	//Examples of each kind of case structure
+	//CamelCase
+	String firstName;
+	
+	//PascalCase
+	String FirstName;
+	
+	//snake_case
+	String first_name;
+	
+	//kebab-case
+	String first-name;
+	
+	//An example of an if statement with the correct structure
+	if(firstName.equals("Conor")) { 
+		System.out.println("Hello Conor");
+	}
+	else {
+		System.out.println("Hello Stranger");
+	}
+	
+	//An example of an if statement with the wrong structure
+	if(firstName.equals("Conor")) 
+	{ 
+		System.out.println("Hello Conor"); } 
+	else {
+		System.out.println("Hello Stranger"); }
+	
+	//Ensure there is only one blank space inbetween each method or statement
+}
+}
